c384bcb6a55c028522d92a85ae0accac5b6e4ae3
JergeRG <rodriguezg.jorgea@gmail.com>
2021-10-12 19:44:55 -0500

    merge

--- amarillo.js
+++ amarillo.js
@@ -9,3 +9,5 @@ console.log("Se termina el tiempoo");
 =======
 >>>>>>> c4b065cb3f430cc59bc5c1016a034679b98d0af5
 >>>>>>> 569bb51218f9577b5e7c7d9e843bb9f449139dca
+console.log("Provechito")
+console.log("Este es un cambio bueno XD")
